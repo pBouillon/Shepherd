1756e62f1b4747d3ea831c28cf238f366152975a
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-03-03 22:14:10 +0100

    Scaffold the Spring project

---
+++ backend/shepherd/src/main/java/com/fis/shepherd/ShepherdApplication.java
@@ -0,0 +1,13 @@
+package com.fis.shepherd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShepherdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShepherdApplication.class, args);
+	}
+
+}
