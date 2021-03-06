081381a9643c49551158a0fb99aa41f4380a5b69
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-03-06 20:20:00 +0100

    Create the Media mapper

---
+++ backend/shepherd/src/main/java/com/fisæ/shepherd/infrastructure/mapping/MediaMapping.java
@@ -0,0 +1,22 @@
+package com.fisæ.shepherd.infrastructure.mapping;
+
+import com.fisæ.shepherd.api.contracts.Media;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface MediaMapping {
+
+    /**
+     * Convert a media entity to its DTO
+     *
+     * @param media The entity to convert
+     *
+     * @return The associated DTO
+     */
+    Media toDto(com.fisæ.shepherd.domain.entities.Media media);
+
+}
