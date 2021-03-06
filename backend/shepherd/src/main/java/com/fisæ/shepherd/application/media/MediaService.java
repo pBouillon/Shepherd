2d1d1a0671b4c38dc4956a18109e6f0388125702
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-03-07 00:05:33 +0100

    Add the logic to retrieve all the medias

--- backend/shepherd/src/main/java/com/fisæ/shepherd/application/media/MediaService.java
+++ backend/shepherd/src/main/java/com/fisæ/shepherd/application/media/MediaService.java
@@ -2,15 +2,20 @@ package com.fisæ.shepherd.application.media;
 
 import com.fisæ.shepherd.api.contracts.MediaDto;
 import com.fisæ.shepherd.application.media.query.GetMediasQuery;
+import com.fisæ.shepherd.domain.entities.Media;
 import com.fisæ.shepherd.infrastructure.mapping.MediaMapper;
+import com.fisæ.shepherd.infrastructure.persistence.repository.MediaRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Define a service able to handle the operations performed on medias
  */
+@Log4j2
 @Service
 public class MediaService implements MediaQueryService {
 
@@ -20,13 +25,20 @@ public class MediaService implements MediaQueryService {
     private final MediaMapper mapper;
 
     /**
+     * DAO to access the persisted {@link Media} entities
+     */
+    private final MediaRepository repository;
+
+    /**
      * Create the service
      *
      * @param mediaMapper Mapper to convert Media entities to their DTO
+     * @param mediaRepository DAO to access the persisted {@link Media} entities
      */
     @Autowired
-    public MediaService(MediaMapper mediaMapper) {
+    public MediaService(MediaMapper mediaMapper, MediaRepository mediaRepository) {
         mapper = mediaMapper;
+        repository = mediaRepository;
     }
 
     /**
@@ -34,8 +46,11 @@ public class MediaService implements MediaQueryService {
      */
     @Override
     public List<MediaDto> getMedias(GetMediasQuery query) {
-        // TODO: implementation, logging and mapping
-        return null;
+        List<Media> medias = repository.findAll();
+
+        log.info("Retrieved {} medias", medias.size());
+
+        return mapper.toDtos(medias);
     }
 
 }
