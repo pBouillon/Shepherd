7a153d16e284b8a165b835a8ae54edae249fee75
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-03-11 14:39:17 +0100

    Add the homepage component

---
+++ frontend/shepherd/src/app/pages/pages.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomepageComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomepageComponent,
+  ]
+})
+export class PagesModule { }
