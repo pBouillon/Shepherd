38c05099bd1478a3788535e1387c3d5216d1fc67
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-03-18 22:12:48 +0100

    Prepare the search field

--- frontend/shepherd/src/app/pages/medias/medias.component.ts
+++ frontend/shepherd/src/app/pages/medias/medias.component.ts
@@ -1,15 +1,18 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, map, filter, distinctUntilChanged } from 'rxjs/operators';
 import { PaginatedMedias } from 'src/app/models/medias/paginated-medias';
 import { MediaService } from 'src/app/shared/services/media/media.service';
-import { Media } from '../../models/medias/media';
 
 @Component({
   selector: 'app-medias',
   templateUrl: './medias.component.html',
   styleUrls: ['./medias.component.css']
 })
-export class MediasComponent implements OnInit {
+export class MediasComponent implements OnInit, AfterContentInit  {
+
+  public searchForm?: FormGroup;
 
   public page: PaginatedMedias = new PaginatedMedias();
   
@@ -21,11 +24,25 @@ export class MediasComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMedias();
+
+    this.searchForm = new FormGroup({
+      search: new FormControl(),
+    });
+  }
+
+  ngAfterContentInit(): void {
+    this.searchForm?.get('search')?.valueChanges.pipe(
+      debounceTime(500),
+      filter((text: string) => text.length > 2),
+      distinctUntilChanged(),
+    ).subscribe(
+      (search: string) => console.log(search)
+    );
   }
 
   private loadMedias(): void {
     this.mediaService.getMedias({
-      itemsPerPages: 15,
+      itemsPerPages: 9,
       pageId: this.pageIndex
     })
     .subscribe(
@@ -44,3 +61,7 @@ export class MediasComponent implements OnInit {
     this.loadMedias();
   }
 }
+function tap(arg0: (text: string) => string): any {
+  throw new Error('Function not implemented.');
+}
+
